c125782c1a2f33ca53a3184d05a849e05144826f
A-22 <finex0701@gmail.com>
2026-03-12 14:21:30 +0300

    fix:update users

--- database/migrations/2026_03_10_100001_update_users_table.php
+++ database/migrations/2026_03_10_100001_update_users_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(['name', 'email_verified_at', 'password', 'remember_token']);
+            $table->dropColumn(['name','email','email_verified_at', 'password', 'remember_token']);
         });
 
         Schema::table('users', function (Blueprint $table) {
@@ -23,7 +23,16 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
+            // 1. حذف الأعمدة الجديدة
             $table->dropColumn(['role_id', 'full_name', 'password_hash']);
         });
+        Schema::table('users', function (Blueprint $table) {
+        // 2. إعادة الأعمدة الأصلية التي حذفناها في up
+        $table->string('name');
+        $table->string('email')->unique();
+        $table->timestamp('email_verified_at')->nullable();
+        $table->string('password');
+        $table->rememberToken();
+    });
     }
 };
