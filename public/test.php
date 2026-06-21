fc74a57f53301ef5848eafbf7ae2a6314d6d8c02
A-22 <finex0701@gmail.com>
2026-06-21 23:13:14 +0300

    remove debug/test scripts before deployment

--- public/test.php
+++
@@ -1,1 +0,0 @@
-<?php require 'vendor/autoload.php'; \ = require_once 'bootstrap/app.php'; \ = \->make(Illuminate\Contracts\Console\Kernel::class); \->bootstrap(); \ = new App\Http\Controllers\User\MeetingController(); print_r(\->index()->render());
